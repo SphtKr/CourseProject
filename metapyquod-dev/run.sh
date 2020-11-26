fd4fe65ef1917e58dbdd1f4abd885ce75c7e3089
S'pht'Kr <sphtkr@icloud.com>
2020-11-26 12:37:21 -0500

    Added web-pdb

--- metapyquod-dev/run.sh
+++ metapyquod-dev/run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 JUPYTER_PORT=${JUPYTER_PORT:-8888}
+WEB_PDB_PORT=${WEB_PDB_PORT:-5555}
 
-docker run --rm -it -v "`pwd`":/usr/src/app -e JUPYTER_PORT=${JUPYTER_PORT} -p ${JUPYTER_PORT}:${JUPYTER_PORT} metapyquod-dev
+docker run --rm -it -v "`pwd`":/usr/src/app -e JUPYTER_PORT=${JUPYTER_PORT} -p ${JUPYTER_PORT}:${JUPYTER_PORT} -p ${WEB_PDB_PORT}:${WEB_PDB_PORT} metapyquod-dev
 
 #IPADDRESS=docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID)
 
