ed8540ffb8694517dbc18e23f8097f218908c5a1
S'pht'Kr <sphtkr@icloud.com>
2020-12-04 19:51:43 -0500

    Dockerfile optimized to reduce size, run.sh set to use pushed image and python 3.8 version.

--- metapyquod-dev/run.sh
+++ metapyquod-dev/run.sh
@@ -3,7 +3,7 @@
 JUPYTER_PORT=${JUPYTER_PORT:-8888}
 WEB_PDB_PORT=${WEB_PDB_PORT:-5555}
 
-docker run --rm -it -v "`pwd`":/usr/src/app -e JUPYTER_PORT=${JUPYTER_PORT} -p ${JUPYTER_PORT}:${JUPYTER_PORT} -p ${WEB_PDB_PORT}:${WEB_PDB_PORT} metapyquod-dev
+docker run --rm -it -v "`pwd`":/usr/src/app -e JUPYTER_PORT=${JUPYTER_PORT} -p ${JUPYTER_PORT}:${JUPYTER_PORT} -p ${WEB_PDB_PORT}:5555 sphtkr/metapyquod-dev:python3.8
 
 #IPADDRESS=docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID)
 
